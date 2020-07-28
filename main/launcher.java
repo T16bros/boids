92314723f3ebda100de195d539c0f2588e548e59
Mr. Programmer <thunderstaff123@gmail.com>
2020-07-27 22:26:18 -0400

    setup workspace

---
+++ main/launcher.java
@@ -0,0 +1,11 @@
+package main;
+
+import util.Window;
+
+public class launcher {
+	
+	public static void main(String[] args) {
+		new run(new Window("Frame", 800, 600));
+	}
+
+}
---
+++ main/run.java
@@ -0,0 +1,15 @@
+package main;
+
+import util.Window;
+import util.Loop;
+
+public class run {
+	
+	public Window window;
+
+	public run(Window window) {
+		this.window = window;
+		canvas = window.getCanvas();
+	}
+
+}
